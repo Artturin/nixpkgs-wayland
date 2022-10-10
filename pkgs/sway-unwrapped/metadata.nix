c3788f60ede8cfcd7567cc6ac63082384eb14b29
Artturin <Artturin@artturin.com>
2022-10-10 17:08:49 +0300

    auto-update(manual): sway-unwrapped: c7bced9329ede5f0f3732d32e52316f708be62a6 => 7a6afc51995aebea4c78d87d3297806e31a66563

--- pkgs/sway-unwrapped/metadata.nix
+++ pkgs/sway-unwrapped/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/swaywm/sway";
   branch = "master";
   upattr = "sway-unwrapped";
-  rev = "c7bced9329ede5f0f3732d32e52316f708be62a6";
-  sha256 = "sha256-39YkSVdSB4u4gAefUixAq8ITWzqtK0Oz53WC5ZTzzTY=";
+  rev = "7a6afc51995aebea4c78d87d3297806e31a66563";
+  sha256 = "sha256-j+LQTpEVVrPrgSPKMZHJMKam8MbStp4Mw3YvcKUJkfw=";
 }
