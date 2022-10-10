eb5bc1463b485e6e5ed60df53968449c05bfad72
Artturin <Artturin@artturin.com>
2022-10-10 17:21:42 +0300

    drm_info: add scdoc

--- pkgs/drm_info/default.nix
+++ pkgs/drm_info/default.nix
@@ -1,12 +1,13 @@
-args_@{ lib, fetchFromGitLab, drm_info, ... }:
+args_@{ lib, fetchFromGitLab, drm_info, scdoc, ... }:
 
 let
   metadata = import ./metadata.nix;
   # remove fetchFromGitLab once nixpkgs uses fetchFromGitLab for drm_info
-  ignore = [ "drm_info" "fetchFromGitLab" ];
+  ignore = [ "drm_info" "fetchFromGitLab" "scdoc" ];
   args = lib.filterAttrs (n: _v: (!builtins.elem n ignore)) args_;
 in
-(drm_info.override args).overrideAttrs (_old: {
+(drm_info.override args).overrideAttrs (old: {
+  buildInputs = [ scdoc ] ++ old.buildInputs;
   version = metadata.rev;
   src = fetchFromGitLab {
     inherit (metadata) domain owner repo rev sha256;
